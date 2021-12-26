12fda39f247b522e8df06d6809c04a51a53ce5e4
satyanarayana25 <satyanarayana25@gmail.com>
2021-12-26 20:25:53 +0530

    3 new ml model add

--- model.py
+++ model.py
@@ -1,3 +1,13 @@
 # model for all fraud detection
 
 print("All ML model are add here")
+
+def moodel1():
+    print("adding code for ml model1")
+
+
+def moodel2():
+    print("adding code for ml model2")
+
+def moodel3():
+    print("adding code for ml model3")
